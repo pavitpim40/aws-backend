c62e125fdbed1097a01e13a7ef2bed93e849d01c
pavitpim40 <pavitpim@gmail.com>
2022-06-29 13:18:35 +0700

    test CI/CD

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello Backend" });
+  res.json({ message: "Hello Backend from AWS" });
 });
 
 app.listen(3000);
